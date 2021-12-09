57cf7bf25fc8d8fb93c3d2ce0f7de2f5f7f8b702
ItzDCraft <donovancf12380@gmail.com>
2021-12-09 21:32:34 +0000

    Update main.py
    Cambié el par de rollos

--- ETS2/main.py
+++ ETS2/main.py
@@ -1,9 +1,10 @@
 minimo = int (input("Dime el mínimo ")) #Pedimos el minimo
 maximo = int (input("Dime el máximo ")) #Pedimos el máximo
-for minimo in range(maximo + 1): #Creamos el bucle
-    if minimo % 2 == 0: #Comprobación (% 2 == 0: par; % 2 != 0: impar;)
-        print(str(minimo) + " es par") #Imprimimos el resultado
-    else:
-        print(str(minimo) + " es impar") #Imprimimos el resultado
-print("Terminado") #Imprimimos cuando se acaba el bucle.
 
+if minimo > 0 or maximo > 1:
+    for i in range(minimo, maximo + 1): #Creamos el bucle
+        if minimo % 2 == 0: #Comprobación (% 2 == 0: par; % 2 != 0: impar;)
+            print(str(minimo) + " es par") #Imprimimos el resultado
+        else:
+            print(str(minimo) + " es impar") #Imprimimos el resultado
+print("Terminado") #Imprimimos cuando se acaba el bucle.
