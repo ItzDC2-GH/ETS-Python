b17a75d4130000f13b95804790dc951a450540c7
ItzDCraft <donovancf12380@gmail.com>
2021-12-09 22:26:15 +0000

    Correción de varios errores

--- ETS2/main.py
+++ ETS2/main.py
@@ -1,10 +1,12 @@
 minimo = int (input("Dime el mínimo ")) #Pedimos el minimo
 maximo = int (input("Dime el máximo ")) #Pedimos el máximo
 
-if minimo > 0 or maximo > 1:
+if minimo > 0 or maximo > 1: #Comprobamos que sea un número positivo
     for i in range(minimo, maximo + 1): #Creamos el bucle
         if minimo % 2 == 0: #Comprobación (% 2 == 0: par; % 2 != 0: impar;)
             print(str(minimo) + " es par") #Imprimimos el resultado
         else:
             print(str(minimo) + " es impar") #Imprimimos el resultado
+else:
+    print("Tienes que proporcionar un parámetro positivo")
 print("Terminado") #Imprimimos cuando se acaba el bucle.
--- ETS3/main.py
+++ ETS3/main.py
@@ -1,19 +1,12 @@
 minimo = int(input("Dime el mínimo ")) #Pedimos el mínimo
 maximo = int(input("Dime el máximo ")) #Pedimos el máximo
 
-print("Números primos desde el " + str(minimo) + " hasta el " + str(maximo)) 
+print("Números primos desde el " + str(minimo) + " hasta el " + str(maximo))
 
 if minimo >= 1 and minimo < 10000 and maximo < 10000: #Comprobaciones previas
-   for numero in range(minimo, maximo + 1): #Creamos el primer bucle 
-       if numero > 1: #Comprobamos que sea mayor que 1
-           for i in range(2, numero): #Creamos el segundo bucle
-               if(numero % i) == 0: #Comprobamos que de 0 (si da, rompemos la iteración para que salte a la otra.)
-                #    pass
-                #    print(str(numero) + " no es primo")
-                   break
-               else: # Sería primo, se le añade break, para romper el segundo ciclo ya que si no, se imprimiría varias veces la misma variable.
-                   print(str(numero) + " es primo")
-                   break
+    for numero in range(minimo, maximo + 1): #Creamos el bucle
+        if all(numero % i != 0 for i in range(2, numero)): #Chequeamos que todas las condiciones sean correctas.
+            print("El número " + str(numero) + " es primo.") #Imprimimos el resultado
 elif maximo >= 10000 or minimo >= 10000: #Comprobaciones secundarias
     print("No se puede calcular ese número.")
 elif minimo < 1: #Comprobaciones secundarias
